b132964ea1bd86bcae33c5f346b3f5805c9a2b45
kaedenamakura <pvqv315@yahoo.co.jp>
2026-01-24 22:25:58 +0900

    fix: 本番環境での表示エラー(base path)を修正

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
+  /**
+   * base:
+   */
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
   ],
+
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/setupTests.js'],
+  },
 })
